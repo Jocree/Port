763195c7a462ab1f4f3f1a89327e32fcf863139b
Jocree <108307710+Jocree@users.noreply.github.com>
2023-12-12 16:13:50 -0300

    Update heheheha

--- potifolio/js/disc.js
+++ potifolio/js/disc.js
@@ -1,15 +1,1 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-const token = 'MTE3MTM2OTgyMDU3MTMxMjE0OA.GHaw5V.DqSgdlHDj3a-YoeI1OXzYaZ9EBRv6P0h8EnKUQ'; // Substitua 'SEU_TOKEN_AQUI' pelo seu token
 
-client.on('ready', () => {
-  console.log(`Conectado como ${client.user.tag}!`);
-});
-
-client.login(token);
-
-client.on('ready', () => {
-    console.log(`Conectado como ${client.user.tag}!`);
-    console.log(`Status Online: ${client.user.presence.status}`);
-  });
-  
